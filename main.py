53d06dc11b1fdf232bc14e3a93dd523231e9ea83
kai <kai.sarkar@proton.me>
2025-08-20 23:02:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+MY_API_KEY = "423b47d5b3cd4d3f646f94d07ba0570c"
+ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+params = {
+    "lat": 22.899701,
+    "lon": 88.394763,
+    "appid": MY_API_KEY,
+}
+
+with requests.get(ENDPOINT, params=params) as response:
+    response.raise_for_status()
+    data = response.json()
+    print(data)
